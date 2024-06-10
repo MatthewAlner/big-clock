a5812f552a226e514dc89c1f6d6d6fe0e7f28cbe
Matthew Alner <matthewalner@googlemail.com>
2024-06-10 20:00:12 +0100

    Removed unused NgxMarqueeModule dependency
    The NgxMarqueeModule import has been removed from the scrolling-message component.

--- src/app/pages/home-page/scrolling-message/scrolling-message.component.ts
+++ src/app/pages/home-page/scrolling-message/scrolling-message.component.ts
@@ -1,13 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { NgxMarqueeModule } from 'ngx-marquee';
 
 @Component({
   selector: 'app-scrolling-message',
   standalone: true,
   imports: [
     CommonModule,
-    NgxMarqueeModule,
   ],
   templateUrl: './scrolling-message.component.html',
   styleUrl: './scrolling-message.component.scss'
