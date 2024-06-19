1393a954cb57644667b6a40bf9f6beb64e6ebc3f
Matthew Alner <matthewalner@googlemail.com>
2024-06-19 17:20:30 +0100

    Refactored version update handling into the service
    Moved the version update handling logic from HomePageComponent to UpdateService. This includes the removal of HotToastService dependency and simplification of version event handling in HomePageComponent. Now, all toast notifications related to version updates are handled within UpdateService itself.

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -7,7 +7,6 @@ import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 import { faClock } from '@fortawesome/free-regular-svg-icons';
 import { faBullhorn, faCircleDown, faCog } from '@fortawesome/free-solid-svg-icons';
 import { NgbTooltip } from '@ng-bootstrap/ng-bootstrap';
-import { HotToastService } from '@ngxpert/hot-toast';
 import { UpdateService } from '../../../shared/services/update.service';
 import { ClockComponent } from './clock/clock.component';
 import { ScrollingMessageComponent } from './scrolling-message/scrolling-message.component';
@@ -33,7 +32,6 @@ export class HomePageComponent implements OnInit {
 
   constructor(
     private destroyRef: DestroyRef,
-    private toast: HotToastService,
     private updateService: UpdateService,
   ) { }
 
@@ -51,32 +49,13 @@ export class HomePageComponent implements OnInit {
     this.updateService.updateAvailable$
       .pipe(takeUntilDestroyed(this.destroyRef))
       .subscribe((versionEvent: VersionEvent) => {
-        console.log({ versionEvent });
-
-        switch (versionEvent.type) {
-          case 'VERSION_READY':
-            // An event emitted when a new version of the app is available.
-            this.toast.info('New version is ready');
-            this.isNewAppVersionAvailable = true;
-            break;
-          case 'VERSION_INSTALLATION_FAILED':
-            // An event emitted when the installation of a new version failed. It may be used for logging/ monitoring purposes.
-            this.toast.error('New version failed to install');
-            break;
-          case 'VERSION_DETECTED':
-            // An event emitted when the service worker has detected a new version of the app on the server and is about to start downloading it.
-            this.toast.info('New version detected');
-            break;
-          case 'NO_NEW_VERSION_DETECTED':
-            // An event emitted when the service worker has checked the version of the app on the server and it didn't find a new version that it doesn't have already downloaded.
-            this.toast.info('No new version detected');
-            this.isNewAppVersionAvailable = false;
-            break;
-          default:
-            this.toast.warning('Unknown version event');
-            break;
+        if (versionEvent.type === 'VERSION_READY') {
+          this.isNewAppVersionAvailable = true;
         }
 
+        if (versionEvent.type === 'NO_NEW_VERSION_DETECTED') {
+          this.isNewAppVersionAvailable = false;
+        }
       });
   }
 
--- src/shared/services/update.service.ts
+++ src/shared/services/update.service.ts
@@ -1,5 +1,7 @@
-import { Injectable } from '@angular/core';
+import { DestroyRef, Injectable } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { SwUpdate, VersionEvent } from '@angular/service-worker';
+import { HotToastService } from '@ngxpert/hot-toast';
 import { from, Observable } from 'rxjs';
 
 @Injectable({
@@ -10,9 +12,39 @@ export class UpdateService {
   public updateAvailable$: Observable<VersionEvent>;
 
   constructor(
+    private destroyRef: DestroyRef,
     private swUpdate: SwUpdate,
+    private toast: HotToastService,
   ) {
     this.updateAvailable$ = this.swUpdate.versionUpdates;
+
+    this.swUpdate.versionUpdates
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe((versionEvent: VersionEvent) => {
+        console.log({ versionEvent });
+
+        switch (versionEvent.type) {
+          case 'VERSION_READY':
+            // An event emitted when a new version of the app is available.
+            this.toast.info('New version is ready');
+            break;
+          case 'VERSION_INSTALLATION_FAILED':
+            // An event emitted when the installation of a new version failed. It may be used for logging/ monitoring purposes.
+            this.toast.error('New version failed to install');
+            break;
+          case 'VERSION_DETECTED':
+            // An event emitted when the service worker has detected a new version of the app on the server and is about to start downloading it.
+            this.toast.info('New version detected');
+            break;
+          case 'NO_NEW_VERSION_DETECTED':
+            // An event emitted when the service worker has checked the version of the app on the server and it didn't find a new version that it doesn't have already downloaded.
+            this.toast.info('No new version detected');
+            break;
+          default:
+            this.toast.warning('Unknown version event');
+            break;
+        }
+      });
   }
 
   public forceUpdate(): Observable<boolean> {
