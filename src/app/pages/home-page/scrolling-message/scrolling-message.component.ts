8d26e727037e1626cc777ee8a3830cdb9ea5de0e
Matthew Alner <matthewalner@googlemail.com>
2024-06-21 11:00:42 +0100

    Added scrolling message feature
    A new package 'ngx-fast-marquee' has been added to the project dependencies. This package is used to implement a scrolling message feature on the home page. The HTML, SCSS, and TypeScript files of the ScrollingMessageComponent have been updated accordingly. The width of the app-scrolling-message in MessagePageComponent's SCSS file has also been set to 100%.

--- src/app/pages/home-page/scrolling-message/scrolling-message.component.ts
+++ src/app/pages/home-page/scrolling-message/scrolling-message.component.ts
@@ -1,15 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { Component, input } from '@angular/core';
+import { NgxFastMarqueeModule } from 'ngx-fast-marquee';
 
 @Component({
   selector: 'app-scrolling-message',
   standalone: true,
   imports: [
     CommonModule,
+    NgxFastMarqueeModule,
   ],
   templateUrl: './scrolling-message.component.html',
   styleUrl: './scrolling-message.component.scss'
 })
 export class ScrollingMessageComponent {
-  messageText = input.required<string>();
+  public messageText = input.required<string>();
 }
